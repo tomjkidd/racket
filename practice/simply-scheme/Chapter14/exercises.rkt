d03b1a0d989e2da69a093798d622a0b551cc0a8b
unknown <tomjkidd@gmail.com>
2015-07-25 09:52:55 -0400

    Adding more Chapter 14 exercises for Simply Scheme

--- practice/simply-scheme/Chapter14/exercises.rkt
+++ practice/simply-scheme/Chapter14/exercises.rkt
@@ -34,3 +34,46 @@
   (remdup-rec '() sent))
 (trace remdup-rec)
 (remdup '(ob la di ob la da))
+
+#! 14.4
+(define (odds sent)
+  (cond ((empty? sent) '())
+        ((= (count sent) 1) sent)
+        ((>= (count sent) 2) (se (first sent) (odds (bf (bf sent)))))))
+
+(odds '(i lost my little girl))
+
+(define (evens sent)
+  (if (empty? sent)
+      '()
+      (odds (bf sent))))
+
+(evens '(i lost my little girl))
+
+#! 14.5
+(define (letter-count sent)
+  (accumulate + (every (lambda (wd) (count wd)) sent)))
+
+(letter-count '(fixing a hole))
+
+#! 14.6
+(define (member-mod? needle haystack)
+  (cond ((empty? haystack) #f)
+        (else (or (equal? needle (first haystack))
+            (member-mod? needle (bf haystack))))))
+
+(member-mod? 1 '(1 2 3))
+(member-mod? 1 '(2 3 4))
+(member-mod? 'c '(a b c))
+(member-mod? 'd '(a b c))
+
+#! 14.7
+(define (differences sent)
+  (cond ((>= (count sent) 2) (let ((first-num (first sent))
+                                   (second-num (first (bf sent))))
+                               (se (- second-num first-num)
+                                   (differences (bf sent)))))
+        (else '())))
+
+(trace differences)
+(differences '(4 23 9 87 6 12))
