8a2937a8ce0c036ef0d8221603c6044592f5ccfe
unknown <tomjkidd@gmail.com>
2015-08-10 18:11:23 -0400

    Adding Chapter 23 examples for Simply Scheme

--- practice/simply-scheme/Chapter23/exercises.rkt
+++ practice/simply-scheme/Chapter23/exercises.rkt
@@ -33,3 +33,45 @@ v
 (lap 1)
 (lap 2)
 (lap 1)
+
+(define (card-list)
+  (reduce append
+          (map (lambda (suit) (map (lambda (rank) (word suit rank))
+                                   '(a 2 3 4 5 6 7 8 9 10 j q k)))
+               '(h s d c))))
+
+(card-list)
+
+(define (make-deck)
+  (shuffle! (list->vector (card-list)) 51))
+
+(define (shuffle! deck index)
+  (if (< index 0)
+      deck
+      (begin (vector-swap! deck index (random (+ index 1)))
+             (shuffle! deck (- index 1)))))
+
+(define (vector-swap! vector index1 index2)
+  (let ((temp (vector-ref vector index1)))
+    (vector-set! vector index1 (vector-ref vector index2))
+    (vector-set! vector index2 temp)))
+
+(make-deck)
+(make-deck)
+
+(define (list-square numbers)
+  (if (null? numbers)
+      '()
+      (cons (square (car numbers))
+            (list-square (cdr numbers)))))
+
+(define (vector-square numbers)
+  (vec-sq-helper (make-vector (vector-length numbers))
+                 numbers
+                 (- (vector-length numbers) 1)))
+
+(define (vec-sq-helper new old index)
+  (if (< index 0)
+      new
+      (begin (vector-set! new index (square (vector-ref old index)))
+             (vec-sq-helper new old (- index 1)))))
